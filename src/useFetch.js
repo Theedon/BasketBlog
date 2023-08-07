2c5d32caa255a18494f573e60ba0fff319eb582d
Theedon <faniyantoyin@gmail.com>
2023-08-07 13:33:26 +0100

    create custom hook to fetch data

---
+++ src/useFetch.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+
+const useFetch = (jsonFilePath) => {
+  const [data, setData] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch(jsonFilePath)
+      .then((response) => {
+        if (!response.ok) {
+          throw Error("error fetching resource");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+        setError(null);
+      })
+      .catch((error) => {
+        console.log("no");
+        console.log(error);
+        setError(error.message);
+        setLoading(false);
+      });
+  }, [jsonFilePath]);
+
+  return { data, loading, error };
+};
+
+export default useFetch;
