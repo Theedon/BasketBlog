11684a942ebd234f344e0c4ccfac57b0df311f3e
Theedon <faniyantoyin@gmail.com>
2023-08-26 21:11:05 +0100

    add list of five players to every team's page

--- src/TeamInfo.jsx
+++ src/TeamInfo.jsx
@@ -1,11 +1,26 @@
 import { Grid, Box, Typography } from "@mui/material";
 import useFetch from "./useFetch";
 import { useParams } from "react-router-dom";
+import {
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+import { Link } from "react-router-dom";
+import Person3Icon from "@mui/icons-material/Person3";
 
 const TeamInfo = () => {
   const { id } = useParams();
   const url = `https://basketblog-api.vercel.app/teams/${id}`;
   const { data, loading, error } = useFetch(url);
+  const playersUrl = `https://basketblog-api.vercel.app/players/team/${id}`;
+  const {
+    data: playersData,
+    loading: playersLoading,
+    error: playersError,
+  } = useFetch(playersUrl);
 
   return (
     <div>
@@ -24,7 +39,36 @@ const TeamInfo = () => {
           <Grid item xs={4}></Grid>
           <Grid item sx={{ marginTop: 5 }}>
             <Typography variant="h5">Player List</Typography>
-            <Box>{/* have list of players here */}</Box>
+            <Box>
+              {playersError && (
+                <Typography>
+                  Error accessing players data {`\n${error}`}
+                </Typography>
+              )}
+              {playersLoading && <Typography>Loading...</Typography>}
+              {playersData && (
+                <nav aria-label="Team players">
+                  <List>
+                    {playersData.map((player) => (
+                      <ListItem key={player.id}>
+                        <ListItemButton
+                          component={Link}
+                          to={`/player/${player.playerId}`}
+                        >
+                          <ListItemIcon>
+                            <Person3Icon />
+                          </ListItemIcon>
+                          <ListItemText
+                            primary={player.name}
+                            secondary={player.nationality}
+                          ></ListItemText>
+                        </ListItemButton>
+                      </ListItem>
+                    ))}
+                  </List>
+                </nav>
+              )}
+            </Box>
           </Grid>
           <Grid item xs={4}></Grid>
         </Grid>
