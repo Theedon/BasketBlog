f0d548724c9ee32ab3ffc43a49f5710ccb4fc66d
Theedon <faniyantoyin@gmail.com>
2023-08-26 21:48:25 +0100

    make some design changes

--- src/TeamInfo.jsx
+++ src/TeamInfo.jsx
@@ -10,6 +10,7 @@ import {
 } from "@mui/material";
 import { Link } from "react-router-dom";
 import Person3Icon from "@mui/icons-material/Person3";
+import styles from "./PlayerInfo.module.css";
 
 const TeamInfo = () => {
   const { id } = useParams();
@@ -23,54 +24,60 @@ const TeamInfo = () => {
   } = useFetch(playersUrl);
 
   return (
-    <div>
-      {loading && <Typography variant="h3">Loading...</Typography>}
+    <div className={styles.playerInfoContainer}>
+      {loading && <Typography variant="h2">Loading...</Typography>}
       {error && (
-        <Typography variant="h3">Error retrieving team data</Typography>
+        <Typography variant="h2">Error retrieving team data</Typography>
       )}
       {data && (
         <Grid container justifyContent="center">
-          <Grid item xs={3}></Grid>
-          <Grid item sx={{ marginTop: 5 }} xs={6} className="blogInfoGrid">
-            <Typography className="blogInfo">{data.info}</Typography>
-          </Grid>
-          <Grid item xs={3}></Grid>
+          <Grid item xs={12} sm={6} className={styles.playerInfoGrid}>
+            <Typography variant="h5" gutterBottom>
+              Team Wiki:
+            </Typography>
 
-          <Grid item xs={4}></Grid>
-          <Grid item sx={{ marginTop: 5 }}>
-            <Typography variant="h5">Player List</Typography>
-            <Box>
-              {playersError && (
-                <Typography>
-                  Error accessing players data {`\n${error}`}
-                </Typography>
-              )}
-              {playersLoading && <Typography>Loading...</Typography>}
-              {playersData && (
-                <nav aria-label="Team players">
-                  <List>
-                    {playersData.map((player) => (
-                      <ListItem key={player.id}>
-                        <ListItemButton
-                          component={Link}
-                          to={`/player/${player.playerId}`}
-                        >
-                          <ListItemIcon>
-                            <Person3Icon />
-                          </ListItemIcon>
-                          <ListItemText
-                            primary={player.name}
-                            secondary={player.nationality}
-                          ></ListItemText>
-                        </ListItemButton>
-                      </ListItem>
-                    ))}
-                  </List>
-                </nav>
-              )}
-            </Box>
+            <Typography variant="h6" gutterBottom>
+              {data.info}
+            </Typography>
           </Grid>
-          <Grid item xs={4}></Grid>
+
+          <Grid
+            item
+            className={styles.playerInfoGrid}
+            sx={{ marginLeft: "2px" }}
+          >
+            {playersError && (
+              <Typography>
+                Error accessing players data {`\n${error}`}
+              </Typography>
+            )}
+            {playersLoading && <Typography>Loading...</Typography>}
+            {playersData && (
+              <nav aria-label="Team players">
+                <Typography variant="h5" gutterBottom>
+                  Players List
+                </Typography>
+                <List>
+                  {playersData.map((player) => (
+                    <ListItem key={player.id}>
+                      <ListItemButton
+                        component={Link}
+                        to={`/player/${player.playerId}`}
+                      >
+                        <ListItemIcon>
+                          <Person3Icon />
+                        </ListItemIcon>
+                        <ListItemText
+                          primary={player.name}
+                          secondary={player.nationality}
+                        ></ListItemText>
+                      </ListItemButton>
+                    </ListItem>
+                  ))}
+                </List>
+              </nav>
+            )}
+          </Grid>
         </Grid>
       )}
     </div>
